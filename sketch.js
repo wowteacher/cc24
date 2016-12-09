e0fc7ac68e975f11a33bc22223d3ee7115b152f2
wowteacher <francesca.sampietro@gmail.com>
2016-12-09 15:50:27 +0100

    griglia di pixel
    tolgo i riferimenti al singolo pixel e disegno una griglia di quadrati.

--- sketch.js
+++ sketch.js
@@ -16,20 +16,17 @@ function setup() {
 }
 
 function draw() {
+var yoff = 0;
 
-
-  var yoff = 0;
-  loadPixels();
-  for (var y = 0; y < height; y++) { //colonne dell'array di pixel
+  for (var y = 0; y < rows; y++) { //righe dell'array di pixel
     var xoff = 0;											// slittamento in orizzontale del noise
-    for (var x = 0; x < width; x++) { //righe dell'array di pixel
+    for (var x = 0; x < cols; x++) { //colonne dell'array di pixel
       var index = (x + y * width) * 4; // visualizzazione di ogni punto in base alla larghezza
       var r = noise(xoff, yoff) * 255; // aggiungo il noise per l'interpolazione
-      pixels[index + 0] = r;
-      pixels[index + 1] = r;
-      pixels[index + 2] = r;
-      pixels[index + 3] = 255;
+   
       xoff += inc;
+   	fill(random(255));
+   	rect(x * scl, y * scl, scl,scl); // griglia di pixel
     }
     yoff += inc;
   }
